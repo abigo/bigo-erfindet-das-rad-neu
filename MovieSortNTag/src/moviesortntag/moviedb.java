371a1b7ba06a99f73510273f189d25ff04476f22
abigo <n.d.911@aon.at>
2010-10-02 22:10:18 +0200

    update

--- MovieSortNTag/src/moviesortntag/moviedb.java
+++ MovieSortNTag/src/moviesortntag/moviedb.java
@@ -6,6 +6,8 @@
 package moviesortntag;
 
 import com.sun.corba.se.impl.encoding.WrapperInputStream;
+import com.sun.org.apache.bcel.internal.util.ByteSequence;
+import com.sun.org.apache.bcel.internal.util.JavaWrapper;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
@@ -15,6 +17,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,21 +30,50 @@ import java.util.logging.Logger;
  */
 public class moviedb {
 
-    public String movieGetInfo(String name)
+    public static final String apikey = "e154c7431a246c9030268275b4e81b8e";
+
+
+
+
+//    public static String movieSearch(String name)
+//    {
+//        String xml = movieSearchXml(name);
+//
+//
+//    }
+
+    public static String movieSearchXml(String name)
+    {
+        return request("Movie.search",name);
+    }
+
+    public static String movieGetInfo(int id)
+    {
+        return request("Movie.getInfo", String.valueOf(id));
+    }
+
+
+    public static String request(String method, String args)
     {
         try {
-            FileReader r = new FileReader("http://www.google.de");
-            FileInputStream fis = new FileInputStream("http://www.google.de");
+            InputStream s = getStream(method, args);
 
-            BufferedReader b = new BufferedReader(r);
+            BufferedInputStream r = new BufferedInputStream(s);
+
+
+            InputStreamReader isr = new InputStreamReader(s);
+
+            BufferedReader br = new BufferedReader(isr);
+
+
 
             String text = "";
             String line = null;
-            while(null != (line = b.readLine()))
+            while(null != (line = br.readLine()))
             {
-                text += line;
+                text += line + "\n";
             }
-            
+
             return text;
 
         } catch (Exception ex) {
@@ -47,7 +82,18 @@ public class moviedb {
 
         return null;
 
+    }
 
+    public static InputStream getStream(String method, String args)
+    {
+        try {
+            URL url = new URL("http://api.themoviedb.org/2.1/" + method + "/en/xml/" + apikey + "/" + args);
+            URLConnection conn = url.openConnection();
+            return conn.getInputStream();
+        } catch (Exception ex) {
+            Logger.getLogger(moviedb.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
 
 }
